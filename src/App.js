eaa0ac156339da16c183eb972fb01ff34922be5b
owin40 <116938413+owin40@users.noreply.github.com>
2023-05-27 11:37:21 +0700

    file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax pages={2} style={{ top: "0", left: "0" }} class="animation">
+        <ParallaxLayer offset={0} speed={0.2}>
+          <div class="animation_layer parallax" id="pg1-bg"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.3}>
+          <div class="animation_layer parallax" id="title"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.4}>
+          <div class="animation_layer parallax" id="acc11"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.2}>
+          <div class="animation_layer parallax" id="overlay"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={1.0}>
+          <div class="animation_layer parallax" id="acc22"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.3}>
+          <div class="animation_layer parallax" id="title-stroke"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.2}>
+          <div class="animation_layer parallax" id="pg2-bg"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={1.5}>
+          <div class="animation_layer parallax" id="acc33"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={2.5}>
+          <div class="animation_layer parallax" id="acc44"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.35}>
+          <div class="animation_layer parallax" id="acc55"></div>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
